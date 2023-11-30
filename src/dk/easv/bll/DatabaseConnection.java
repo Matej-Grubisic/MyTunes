8f3e3bbca870c2c7bfb2a1124990dc0c230f4bd4
Dancho28bg <y.rusev28@gmail.com>
2023-11-30 11:53:49 +0100

    Update DatabaseConnection.java
    testing connection between database and program

--- src/dk/easv/bll/DatabaseConnection.java
+++ src/dk/easv/bll/DatabaseConnection.java
@@ -5,11 +5,22 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String JDBC_URL = "jdbc:sqlserver://10.176.111.34\\EASV-DB4:1433;databaseName=mytunes_groupg";
+    private static final String JDBC_URL = "jdbc:sqlserver://10.176.111.34\\EASV-DB4:1433;databaseName=mytunes_groupg;trustServerCertificate=true";
     private static final String USER = "CSe2023b_e_6";
     private static final String PASSWORD = "CSe2023bE6#23";
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+    public static void main(String[] args) {
+        try {
+            testDatabaseConnection();
+            System.out.println("Database connection successful!");
+        } catch (SQLException e) {
+            System.err.println("Error connecting to the database:");
+            e.printStackTrace();
+        }
+    }
+
+    private static void testDatabaseConnection() throws SQLException {
+        try (Connection connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD)) {
+        }
     }
 }
