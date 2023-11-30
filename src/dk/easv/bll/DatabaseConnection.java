9e2cd441593f2c979b437c76ade022a094e87f42
Dancho28bg <y.rusev28@gmail.com>
2023-11-30 11:43:35 +0100

    connect database to Java files

---
+++ src/dk/easv/bll/DatabaseConnection.java
@@ -0,0 +1,15 @@
+package dk.easv.bll;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String JDBC_URL = "jdbc:sqlserver://10.176.111.34\\EASV-DB4:1433;databaseName=mytunes_groupg";
+    private static final String USER = "CSe2023b_e_6";
+    private static final String PASSWORD = "CSe2023bE6#23";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+    }
+}
