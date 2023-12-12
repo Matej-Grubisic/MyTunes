668e7738a7a291d8c5624c2f7d3d3d322bb37524
Matej Grubišić <grubisic401@gmail.com>
2023-12-12 11:48:58 +0100

    Changes almost nothing minor thing

--- src/dk/easv/gui/newPlaylistController.java
+++ src/dk/easv/gui/newPlaylistController.java
@@ -1,27 +1,23 @@
 package dk.easv.gui;
 import dk.easv.be.Playlist;
-import dk.easv.bll.DatabaseConnection;
 import dk.easv.dal.PlaylistDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 
-import javax.sound.sampled.UnsupportedAudioFileException;
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
-import java.sql.*;
-import java.util.HashMap;
+
 public class newPlaylistController {
     private final Desktop desktop = Desktop.getDesktop();
+    public TextField playlistNameField;
+    public Button cancelButton;
     @FXML
     private Button saveButton;
-    @FXML
-    private TextField playlistNameField;
+    
     private final PlaylistDAO PlaylistDAO = new PlaylistDAO();
+
     @FXML
     private void savePlaylist() {
         String playlistName = playlistNameField.getText();
